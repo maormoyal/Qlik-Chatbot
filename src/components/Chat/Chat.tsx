cc86206a8c23005b87b0939528a25c3ad05a7da6
maor moyal <maorkab@gmail.com>
2024-07-14 11:07:03 +0300

    Refactor Chat component and ChatContext, update messages and styles

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,12 +1,25 @@
+import { useEffect } from 'react';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 import MessageList from '../MessageList/MessageList';
 import SideBar from '../SideBar/SideBar';
 import styles from './Chat.module.scss';
 import { useChatContext } from './ChatContext';
+import { User } from './Chat.types';
 
-const Chat: React.FC = () => {
-  const { showSidebar } = useChatContext();
+interface UserProps {
+  user: User;
+}
+
+const Chat: React.FC<UserProps> = ({ user }) => {
+  const { showSidebar, setUser } = useChatContext();
+
+  useEffect(() => {
+    setUser({
+      name: user.name,
+      avatar: user.avatar,
+    });
+  }, [setUser]);
 
   return (
     <div className={styles.chatContainer}>
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './components/App';
+
 import { ChatProvider } from './components/Chat/ChatContext';
+
+import userAvatar from './assets/maor-avatar.jpeg';
+import Chat from './components/Chat/Chat';
+
+const user = {
+  name: 'Maor Moyal',
+  avatar: userAvatar,
+};
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChatProvider>
-      <App />
+      <Chat user={user} />
     </ChatProvider>
   </React.StrictMode>
 );
